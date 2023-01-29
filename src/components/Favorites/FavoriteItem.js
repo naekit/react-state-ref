185bba8168c09bec3f77cb9ce66f10067b7b175e
naekit <mykyta.onishchuck@code.berlin>
2023-01-29 12:52:41 -0500

    component formatting

--- src/components/Favorites/FavoriteItem.js
+++ src/components/Favorites/FavoriteItem.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react"
 
-import Card from '../UI/Card';
-import './FavoriteItem.css';
+import Card from "../UI/Card"
+import "./FavoriteItem.css"
 
-const FavoriteItem = props => {
-  return (
-    <Card style={{ marginBottom: '1rem' }}>
-      <div className="favorite-item">
-        <h2>{props.title}</h2>
-        <p>{props.description}</p>
-      </div>
-    </Card>
-  );
-};
+const FavoriteItem = (props) => {
+	return (
+		<Card style={{ marginBottom: "1rem" }}>
+			<div className="favorite-item">
+				<h2>{props.title}</h2>
+				<p>{props.description}</p>
+			</div>
+		</Card>
+	)
+}
 
-export default FavoriteItem;
+export default FavoriteItem
